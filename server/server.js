55c4c739b33dcde171f73e401974e5c190504cfa
faris Naser <solonaser9@gmail.com>
2026-07-01 12:09:30 -0700

    feat:added  the backend route in the back

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 import express from "express";
+import productZRoute from "./routes/product.js";
 const app = express();
+app.use(express.json());
+app.use("/api/products", productZRoute);
 const port = 2300;
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
