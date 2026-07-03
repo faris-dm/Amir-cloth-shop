f8747b3ce4afe05392f24f956bfd856891817c1d
faris Naser <solonaser9@gmail.com>
2026-07-03 05:42:27 -0700

    added a new path to the   product route

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import productZRoute from "./routes/product.js";
 const app = express();
 app.use(express.json());
-app.use("/api/products", productZRoute);
+app.use("/api/", productZRoute);
 const port = 2300;
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
+
 });
+
