28fba250fc6e6012c66808da236afc7889a510cf
faris Naser <solonaser9@gmail.com>
2026-07-03 05:44:13 -0700

    added a new  file in the main app and  updated the packages and added some   txt files  to  doc the process

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,16 @@ import Products from "./pages/products";
 import Accordion from "./pages/Accordion";
 import counter from "./pages/counter";
 import Props from "./pages/props/props";
+import Sidebar from "./pages/componts/sidebar"
 
 import Filter from "./pages/Js_Filter";
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
-       {/*   <Props /> */}
-      <Home />
+      {/*   <Props /> */}
+      {/* <Home /> */}
+      <Sidebar />
       {/* <Accordion /> */}
       {/* <counter /> */}
       {/* <Products /> */}
