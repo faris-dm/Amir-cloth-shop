d6742a4310fd269b758761c668398e868da58403
faris Naser <solonaser9@gmail.com>
2026-07-04 05:15:36 -0700

    added a welcome message    in the server

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ app.use(express.json());
 app.use("/api/", productZRoute);
 const port = 2300;
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-
+app.get("/", (req, res) => {
+  res.json({ message: "It is  working" });
 });
 
+app.listen(port, () => {
+  console.log(`http://:localhost:${port}`);
+});
