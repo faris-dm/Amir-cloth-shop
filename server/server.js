9c890e9332d6681f253ddf7fb8da9cce271b974a
faris Naser <solonaser9@gmail.com>
2026-07-20 05:48:18 -0700

    feat:added a toute totest the db in the main server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import productZRoute from "./routes/product.js";
-import User from "./routes/User.js"
+import User from "./routes/User.js";
+
+// import Pool from "./config/db.js";
+import Pool from "./config/db.js";
 const app = express();
 app.use(express.json());
 app.use("/api/", productZRoute);
@@ -9,6 +12,17 @@ const port = 2300;
 
 app.get("/", (req, res) => {
   res.json({ message: "It is  working Now" });
+});
+
+app.get("/dbase", async (req, res) => {
+  try {
+    const result = await Pool.query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database query failed" });
+  
+  }
 });
 
 app.listen(port, () => {
