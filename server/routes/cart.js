acf7fa1c84787472f1408b2cd61d4191b2e50e5f
faris Naser <solonaser9@gmail.com>
2026-07-30 06:24:56 -0700

    feat:added a post  route in  cart page to add  new  items in the cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -56,7 +56,51 @@ return res.status(200).json({
 
 
 
+  router.post("/add", async (req,res)=> {
+    try {
+      const UserId=req.user.user_id
+      const {id,qantity} =req.body
 
+      if (!id || isNaN(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Valid Product ID is required",
+      });
+    }
+
+    const parsedQuantity = parseInt(qantity, 10);
+    const itemQuantity = !isNaN(parsedQuantity) && parsedQuantity > 0 ? parsedQuantity : 1;
+     const ItemFoundDb= await Pool.query("SELECT id from products WHERE id=$1",[id])
+if(ItemFoundDb.rows.length ===0) {
+return res.status(404).json({
+        success: false,
+        message: "Product does not exist",
+      });
+}
+//   this query is used  to chwck if the user recenntly added  this item in their cart if then we  add the quantity if not just add
+const chackItem=`
+ INSERT INTO  Cart (user_id,item_id,qantity) VALUES ($1,$2,$3)
+ ON CONFLICT (user_id,item_id)
+ DO UPDATE  SET qantity =Cart.qantity+ EXCLUDED.qantity
+ RETURNING *
+`
+
+
+const Result= await Pool.query(chackItem,[UserId,id,itemQuantity])
+return res.status(200).json({
+      success: true,
+      message: "Item added to cart",
+      data: Result.rows[0],
+    });
+      
+    } catch (error) {
+      console.error("Post Cart Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+    }
+  })
 
 
 
