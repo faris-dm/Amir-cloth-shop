210e57b941562bffe9d48588e12695de0c90ffe6
faris Naser <solonaser9@gmail.com>
2026-08-05 05:59:04 -0700

    fix:removed unwanted files  for the  server and uppdated the json packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,22 @@ import Products from "./pages/products";
 import Accordion from "./pages/Accordion";
 import counter from "./pages/counter";
 import Props from "./pages/props/props";
-import Sidebar from "./pages/componts/sidebar"
-
+import Sidebar from "./pages/componts/sidebar";
+import SearchSection from "./pages/componts/search";
 import Filter from "./pages/Js_Filter";
+// this is in the listProducts folder
+import ListProducts from "./pages/listproducts/List"
+
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
-      {/*   <Props /> */}
-      <Home />
-      {/* <Sidebar /> */}
+      {/* <Products /> */}
+      <Navbar />
+      <ListProducts />
+      {/* <Props /> */}
+      {/* <Home /> */}
+
+      <Sidebar />
       {/* <Accordion /> */}
       {/* <counter /> */}
       {/* <Products /> */}
