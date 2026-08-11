d782f06a40ce90b2d8c5354fbd8b925e6fbfcc1b
faris Naser <solonaser9@gmail.com>
2026-08-11 15:26:15 -0700

    feat:minimaized the   main app.jsx  and imported  the pages  to the  route page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,28 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoute from "./pages/props/Route"
 import Navbar from "./pages/Navbar";
-import Home from "./pages/Home";
-import Icon from "./pages/icon";
-import ProductsDetails from "./pages/componts/ProductsDetails";
-import Products from "./pages/products";
-import Accordion from "./pages/Accordion";
-import counter from "./pages/counter";
-import Props from "./pages/props/props";
-import Sidebar from "./pages/componts/sidebar";
-import SearchSection from "./pages/componts/search";
-import Filter from "./pages/Js_Filter";
+import Footer from "./pages/componts/Footer";
+
+// import Filter from "./pages/Js_Filter";
 // this is in the listProducts folder
-import ListProducts from "./pages/listproducts/List";
-import CheckOut from "./pages/componts/CheckOut";
-import Bag from "./pages/componts/Bag";
-import Register    from "./pages/Register/Register"
+
 
 function App() {
   return (
     <div>
-      {/* <Products /> */}
-      <Register />
-      {/* <Navbar /> */}
-      {/* <ProductsDetails /> */}
-      {/* <Bag /> */}
-      {/* <CheckOut /> */}
+   
       {/* <ListProducts /> */}
-      {/* <Props /> */}
-      {/* <Home /> */}
+     
 
-      {/* <Sidebar /> */}
-      {/* <Accordion /> */}
+      <BrowserRouter>
+      <Navbar />
+      <main>
+        <AppRoute />
+      </main>
+
+<Footer />
+      </BrowserRouter>
 
       {/* <Filter /> */}
     </div>
