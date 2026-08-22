c4e613a9eec5b3c1e2dc8121c6fcb6ae8818d0b7
faris Naser <solonaser9@gmail.com>
2026-08-22 11:08:47 -0700

    feat:updated the  seed  file

--- server/seed.js
+++ server/seed.js
@@ -1,26 +1,29 @@
-import express from "express"
+import express from "express";
 // import Api from './api.json' with {   type: "json"}
-import Api from  "./api.json" with {type:"json"}
-const router=express.Router()
+import Api from "./api.js";
+const router = express.Router();
 router.use(express.json());
-import Pool from "./co       nfig/db.js";
+import Pool from "./config/db.js";
 router.use(express.urlencoded({ extended: true }));
 
-
-
-
- async function Seed() {
-    const saveItem =
-    `INSERT INTO products (id,title,price,description,category,image,rating)
+async function Seed() {
+    await Pool.query("TRUNCATE TABLE products RESTART IDENTITY CASCADE;");
+  const saveItem = `INSERT INTO products (id,title,price,description,category,image,rating)
     VALUES ($1,$2,$3,$4,$5,$6,$7)
    
-    `
- for (const items of Api) {
-    await Pool.query(saveItem,[
-        items.id,items.title,items.price,items.description,items.category,items.image,JSON.stringify(items.rating)
-    ])
- }
- console.log("Item Stored succefully")
+    `;
+  for (const items of Api) {
+    await Pool.query(saveItem, [
+      items.id,
+      items.title,
+      items.price,
+      items.description,
+      items.category,
+      items.image,
+      JSON.stringify(items.rating),
+    ]);
+  }
+  console.log("Item Stored succefully");
 }
 
-Seed().catch(err=> console.error("error creating seedind Database",err))
+Seed().catch((err) => console.error("error creating seedind Database", err));
