e50ab48011bf9dcb64c7fc996dd97c2d65893391
faris Naser <solonaser9@gmail.com>
2026-08-23 05:32:43 -0700

    feat:added a  file and path in the server  to acess the images

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import productZRoute from "./routes/product.js";
 import User from "./routes/User.js";
 import Login from "./routes/login.js";
-import cors from "cors"
-// import Pool from "./config/db.js";
+import cors from "cors";
 import Pool from "./config/db.js";
 import Cart from "./routes/cart.js";
-import Order  from "./routes/order.js"
-import RefreshTokens  from "./routes/RefreshToken.js"
+import Order from "./routes/order.js";
+import RefreshTokens from "./routes/RefreshToken.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(
   cors({
@@ -17,7 +23,8 @@ app.use(
   })
 );
 
-app.use(RefreshTokens)
+app.use("/images", express.static(path.join(__dirname, "./images")));
+app.use(RefreshTokens);
 app.use(express.json());
 app.use("/api", productZRoute);
 app.use("/api", User);
@@ -25,13 +32,11 @@ app.use("/api", Login);
 app.use("/api/cart", Cart);
 app.use("/api", Order);
 
-
-
 app.get("/", (req, res) => {
-  res.json({ message: "It is  working Now" });
+  res.json({ message: "It is working Now" });
 });
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`http://:localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
