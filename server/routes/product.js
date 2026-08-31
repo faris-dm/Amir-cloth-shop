66da097a80a642e45237f786457be73ec64feb6c
faris Naser <solonaser9@gmail.com>
2026-08-31 03:51:21 -0700

    feat:updated the products pages  and  added new routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,15 +3,9 @@ const router = express.Router();
 import Role from "../middle/role.js";
 // import ApiNewData from "../api.json" with { type: "json" };
 
-
 import Pool from "../config/db.js";
 
-
-
 router.use(express.json());
-
-
-
 
 // 1. GET ALL PRODUCTS (From PostgreSQL)
 router.get("/products", async (req, res) => {
@@ -24,14 +18,14 @@ router.get("/products", async (req, res) => {
     return res.status(200).json({
       success: true,
       count: resultQuery.rows.length,
-      data: resultQuery.rows
+      data: resultQuery.rows,
     });
   } catch (error) {
     console.error("Database error in GET /products:", error);
     return res.status(500).json({
       success: false,
       message: "Error fetching database items",
-      error: error.message
+      error: error.message,
     });
   }
 });
@@ -49,89 +43,92 @@ router.get("/products/:id", async (req, res) => {
     if (result.rows.length === 0) {
       return res.status(404).json({
         success: false,
-        message: `Product with ID ${id} not found`
+        message: `Product with ID ${id} not found`,
       });
     }
 
     return res.status(200).json({
       success: true,
-      data: result.rows[0]
+      data: result.rows[0],
     });
   } catch (error) {
     console.error("Database error in GET /products/:id:", error);
     return res.status(500).json({
       success: false,
       message: "Error retrieving product details",
-      error: error.message
+      error: error.message,
     });
   }
 });
 
-router.post("/products",  async (req,res)=> {
+router.post("/products", async (req, res) => {
   try {
-    
-   const {title,price,category,description,image,rating}=req.body
+    const { title, price, category, description, image, rating } = req.body;
 
-   if (!title || !price || !description || !category || isNaN((price))) {
-      return res.status(400).json("Please fill in all required information correctly.");
+    if (!title || !price || !description || !category || isNaN(price)) {
+      return res
+        .status(400)
+        .json("Please fill in all required information correctly.");
     }
-    const QuryResult= await Pool.query(
+    const QuryResult = await Pool.query(
       "SELECT title,price,category,description FROM products WHERE title =$1",
       [title]
-    )
+    );
 
-    if(QuryResult.rows.length > 0) {
-       return res.status(409).json({
+    if (QuryResult.rows.length > 0) {
+      return res.status(409).json({
         success: false,
-        message: `Product with  ${title}  found `
+        message: `Product with  ${title}  found `,
       });
-
     }
-     const NewItem=`
+    const NewItem = `
      INSERT INTO products (title,price,category,description,image,rating)
      VALUES ($1,$2,$3,$4,$5,$6)
      RETURNING *;
-   `
-const fromatRatings=  rating ?(typeof rating ==="object" ? JSON.stringify(rating):rating) :null
- const Result= await Pool.query(NewItem,[title,price,category,description,image,fromatRatings])
-     console.log(Result)
-return res.status(201).json({
+   `;
+    const fromatRatings = rating
+      ? typeof rating === "object"
+        ? JSON.stringify(rating)
+        : rating
+      : null;
+    const Result = await Pool.query(NewItem, [
+      title,
+      price,
+      category,
+      description,
+      image,
+      fromatRatings,
+    ]);
+    console.log(Result);
+    return res.status(201).json({
       message: "Item created successfully.",
-      product: Result.rows[0]
+      product: Result.rows[0],
     });
- 
-
   } catch (error) {
     console.error("Postgres Insert Error:", error);
     return res.status(500).json({ error: "Internal server error." });
   }
-  
-})
+});
 
-
-
-
-router.get("/imageDetails/:id", async (req,res)=> {
+router.get("/imageDetails/:id", async (req, res) => {
   try {
-    const  {id}=req.params
+    const { id } = req.params;
     const result = await Pool.query(
       " SELECT * FROM product_images WHERE product_id=$1 ORDER BY position ASC",
       [id]
     );
 
-  if (result.rows.length === 0) {
-    return res.status(404).json({
-      success: false,
-      message: `Product with ID ${id} not found`,
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Product with ID ${id} not found`,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: result.rows,
     });
-  }
-
-  return res.status(200).json({
-    success: true,
-    data: result.rows
-  });
-
-
   } catch (error) {
     console.error("Database error in GET /IMAGES/:id:", error);
     return res.status(500).json({
@@ -139,128 +136,79 @@ router.get("/imageDetails/:id", async (req,res)=> {
       message: "Error retrieving IMAGES details",
       error: error.message,
     });
-    
   }
-})
+});
 
+router.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, price, category, description, image, rating } = req.body;
+  try {
+    if (isNaN(id) || id === "") {
+      return res.status(400).json("Please Insert Valid inputs");
+    }
 
+    const Result = await Pool.query(
+      "SELECT id,title,price,category,description,image,rating  FROM products WHERE id=$1",
+      [id]
+    );
 
+    if (Result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `there is no item  with ${id}  `,
+      });
+    }
+    const ExitingProducet = Result.rows[0];
+    const UpdatedItems = `
+UPDATE products  SET  title=$1,price=$2,category=$3,description=$4,image=$5,rating=$6 WHERE id =$7 RETURNING* `;
 
+    const FinalResult = await Pool.query(UpdatedItems, [
+      title || ExitingProducet.title,
+      price || ExitingProducet.price,
+      category || ExitingProducet.category,
+      description || ExitingProducet.description,
+      image || ExitingProducet.image,
+      rating ? JSON.stringify(rating) : ExitingProducet.rating,
+      ExitingProducet.id,
+    ]);
+    return res.status(200).json({
+      success: true,
+      data: FinalResult.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json("Intrnal Server error");
+  }
+});
 
-router.put("/products/:id", async (req,res)=> {
+router.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (isNaN(id) || id === "") {
+      return res.status(400).json("Please Insert Valid inputs");
+    }
 
-  const {id}=req.params
-   const {title,price,category,description,image,rating} =req.body
- try {
-  
- if(isNaN(id) || id==="") {
-  return res.status(400).json("Please Insert Valid inputs")
- }
+    const result = await Pool.query(
+      `
+  DELETE  FROM products WHERE id=$1 RETURNING *`,
+      [id]
+    );
+    const Rows = result.rows.length;
+    if (Rows === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `there is no item  with ${id}  `,
+      });
+    }
 
-const Result= await Pool.query ("SELECT id,title,price,category,description,image,rating  FROM products WHERE id=$1",[id])
-
-if(Result.rows.length ===0 ) {
- return  res.status(404).json({
-    success:false,
-    message:`there is no item  with ${id}  `
-  })
-}
-const ExitingProducet=Result.rows[0]
-const UpdatedItems=`
-UPDATE products  SET  title=$1,price=$2,category=$3,description=$4,image=$5,rating=$6 WHERE id =$7 RETURNING* `
-   
-const FinalResult= await Pool.query(UpdatedItems,[
-
-  title|| ExitingProducet.title,
-  price ||ExitingProducet.price,
-  category|| ExitingProducet.category,
-  description|| ExitingProducet.description,
-  image||ExitingProducet.image,
-  rating? JSON.stringify(rating) :ExitingProducet.rating,
-    ExitingProducet.id
-])
- return res.status(200).json({
-  success:true,
-  data:FinalResult.rows[0]
- })
- 
-
- } catch (error) {
-  console.error(error)
-  return res.status(500).json("Intrnal Server error")
- }
-})
- 
-
-
-
-
-  router.delete("/products/:id", async (req,res)=> {
- try {
-   const {id}=req.params
-   if(isNaN(id) || id==="") {
-  return res.status(400).json("Please Insert Valid inputs")
- }
-
-const result= await Pool.query(`
-  DELETE  FROM products WHERE id=$1 RETURNING *`,[id])
- const Rows=result.rows.length
-if(Rows ===0) {
- 
-
- return  res.status(404).json({
-    success:false,
-    message:`there is no item  with ${id}  `
-  })
- }
-
-return  res.status(200).json({
-  success:true,
-  data:result.rows[0]
-})
-
-
- } catch (error) {
-  console.error(error)
-  return res.status(500).json("Intrnal server error")
-  
- }
-
-
-
-  }) 
-
-
-
+    return res.status(200).json({
+      success: true,
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json("Intrnal server error");
+  }
+});
 
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
